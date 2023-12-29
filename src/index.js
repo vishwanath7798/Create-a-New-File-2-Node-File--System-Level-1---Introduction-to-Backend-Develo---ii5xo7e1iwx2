f5eb16f810799d5a47fc8dfbf59680d1318c604c
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2023-12-29 11:05:02 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs/promises");
+const fileName = "myfile.txt";
+const fileContent = "Newton School";
+const writeFile = async (fileName, fileContent) => {
+    //Write your code here
+    //Don't change function name
+  
+};
+module.exports =  writeFile ;
